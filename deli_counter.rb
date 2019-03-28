fb307f7b2635d16f20a8669eb7a07e71efffe238
Endy Ranaudo <endyranaudo@gmail.com>
2019-03-28 21:39:54 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,13 +1,13 @@
-# Write your code here.
+require 'pry'
 
 def line(array)
-  if array.length >= 1
+  if array.length > 1
     new_arr = []
-    counter = 1 
+    counter = 1
     array.each do |name|
       new_arr.push("#{counter}. #{name}")
       counter += 1 
-    end 
+    end
     puts "The line is currently: #{new_arr.join(" ")}"
   else
     puts "The line is currently empty."
@@ -20,12 +20,12 @@ def take_a_number(array, name)
   puts "Welcome, #{name}. You are number #{array.length} in line."
 end
 
-
 def now_serving(array)
-  if array.empty?
-    puts "There is nobody waiting to be served!"
-  else
+  binding.pry
+  if array.length > 0
     puts "Currently serving #{array[0]}."
-    array.shift
+    array.shift   
+  else
+    puts "There is nobody waiting to be served!"
   end
 end
